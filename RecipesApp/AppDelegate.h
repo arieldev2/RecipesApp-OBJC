49c42d29990a13de6281a2d1e032181fa2de925c
Ariel Ortiz <arielortiz.dev@gmail.com>
2024-05-01 15:51:26 -0700

    Initial Commit

---
+++ RecipesApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  RecipesApp
+//
+//  Created by Ariel Ortiz on 5/1/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RecipesApp/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  RecipesApp
+//
+//  Created by Ariel Ortiz on 5/1/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
